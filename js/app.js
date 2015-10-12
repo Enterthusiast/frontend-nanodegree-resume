53e867969b57b3bf42d81e793262eb4b1d22575f
Enterthusiast <bernard.damien@gmail.com>
2015-10-13 01:05:36 +0200

    MVO refactor done

---
+++ js/app.js
@@ -0,0 +1,529 @@
+var model = {
+	"googlemaps" : {
+		"map" : {},
+		"locations" : {}
+	},
+	"bio" : {
+		"name" : "Damien Bernard",
+		"role" : "Web Developper",
+		"welcomeMessage" : "Hi, welcome to my portfolio",
+		"contacts" : {
+			"mobile" : "0787....63",
+			"email" : "bernard.damien@gmail.com",
+			"github" : "Enterthusiast",
+			"twitter" : "Enterthusiast",
+			"location" : "Paris, France"
+		},
+		"skills" : ["HTML", "CSS", "JS", "User oriented", "Organized", "Quick learner"],
+		"biopic" : "images/my_picture.jpg",
+	},
+	"education" : {
+		"schools" : [
+		    {
+		        "name" : "EFREI",
+		        "location" : "Villejuif, France",
+		        "degree" : "Engineer",
+		        "majors" : ["Software Engineer"],
+		        "dates" : 2008,
+		        "url" : "http://www.efrei.fr"
+		    }
+		],
+		"onlineCourses" : [
+		    {
+		        "title" : "Web Developper Nanodegree",
+		        "school" : "Udacity",
+		        "dates" : 2015,
+		        "url" : "https://www.udacity.com"
+		    }
+		],
+	},
+	"projects" : {
+		"projects" : [
+			{
+				"title" : "Wargame - Red Dragon",
+				"dates" : "2014",
+				"description" : "Real Time Strategy Game",
+				"images" : ["images/wargame.jpg"]
+			},
+			{
+				"title" : "Naughty Boy",
+				"dates" : "2013",
+				"description" : "Shooter",
+				"images" : ["images/naughtyboy.jpg"]
+			},
+			{
+				"title" : "House of the Dead: Overkill - The Lost Reels",
+				"dates" : "2013",
+				"description" : "Shooter",
+				"images" : ["images/hod.jpg"]
+			},
+			{
+				"title" : "Rakoo's Adventure",
+				"dates" : "2013",
+				"description" : "Runner Game",
+				"images" : ["images/rakoo.jpg"]
+			},
+			{
+				"title" : "Assassin's Creed 3",
+				"dates" : "2012",
+				"description" : "Action & Adventure Game",
+				"images" : ["images/ac3.jpg"]
+			},
+			{
+				"title" : "The Black Eyed Peas Experience",
+				"dates" : "2011",
+				"description" : "Dance Game",
+				"images" : ["images/bepexp.jpg"]
+			},
+			{
+				"title" : "Driver: San Francisco",
+				"dates" : "2011",
+				"description" : "Driving Game",
+				"images" : ["images/driversf.jpg"]
+			},
+			{
+				"title" : "Michael Jackson: The Experience",
+				"dates" : "2010",
+				"description" : "Dance Game",
+				"images" : ["images/mjexp.jpg"]
+			},
+			{
+				"title" : "Just Dance 2",
+				"dates" : "2010",
+				"description" : "Dance Game",
+				"images" : ["images/justdance2.jpg"]
+			},
+			{
+				"title" : "Just Dance",
+				"dates" : "2009",
+				"description" : "Dance Game",
+				"images" : ["images/justdance.jpg"]
+			},
+			{
+				"title" : "The Cursed Crusade",
+				"dates" : "2008",
+				"description" : "Action Game",
+				"images" : ["images/cursedcrusade.jpg"]
+			},
+			{
+				"title" : "Cocoto Festival",
+				"dates" : "2008",
+				"description" : "Shooter Game",
+				"images" : ["images/cocotofestival.jpg"]
+			},
+			{
+				"title" : "Little Folk of Faery",
+				"dates" : "2008",
+				"description" : "Casual Adventure & Management Game",
+				"images" : ["images/lfof.jpg"]
+			}
+		],
+	},
+	"work" : {
+		"jobs" : [
+			{
+				"employer" : "Tap4Fun",
+				"title" : "Game Designer",
+				"location" : "Paris, France",
+				"dates" : "10/2014 - now",
+				"description" : "Design the game mechanics and UIs"
+			},
+			{
+				"employer" : "Novaquark",
+				"title" : "Lead Game Designer",
+				"location" : "Paris, France",
+				"dates" : "04/20014 - 09/2014",
+				"description" : "Design the game mechanics"
+			},
+			{
+				"employer" : "Eugen Sytems",
+				"title" : "Senior Level Designer",
+				"location" : "Paris, France",
+				"dates" : "02/20014 - 04/2014",
+				"description" : "Design a part of the game experience"
+			},
+			{
+				"employer" : "Streum On Studio",
+				"title" : "Level Designer",
+				"location" : "Paris, France",
+				"dates" : "11/2013 - 12/2013",
+				"description" : "Design a part of the game experience"
+			},
+			{
+				"employer" : "Heliceum",
+				"title" : "Lead Game & Level Designer",
+				"location" : "Paris, France",
+				"dates" : "04/2013 - 10/2013",
+				"description" : "Manage a team, manage tasks, project review, design game mechanics & experience"
+			},
+			{
+				"employer" : "Old Skull Game",
+				"title" : "Game & Level Designer",
+				"location" : "Paris, France",
+				"dates" : "04/2013 - 04/2013",
+				"description" : "design game mechanics & experience"
+			},
+			{
+				"employer" : "LISAA",
+				"title" : "Level Design Teacher",
+				"location" : "Paris, France",
+				"dates" : "03/2013 - 04/2013",
+				"description" : "Teach level design"
+			},
+			{
+				"employer" : "Magic Pockets",
+				"title" : "Lead Level Designer & Game Designer",
+				"location" : "Paris, France",
+				"dates" : "09/2012 - 02/2014",
+				"description" : "Design the game experience and a part of the game design"
+			},
+			{
+				"employer" : "Ubisoft",
+				"title" : "Level Designer",
+				"location" : "Quebec City, Canada",
+				"dates" : "01/2012 - 08/2014",
+				"description" : "Design a part of the game experience"
+			},
+			{
+				"employer" : "Ubisoft",
+				"title" : "Lead Level Designer",
+				"location" : "Quebec City, Canada",
+				"dates" : "03/2011 - 12/2011",
+				"description" : "Design the game experience"
+			},
+			{
+				"employer" : "Ubisoft",
+				"title" : "Level Designer Scripter",
+				"location" : "Newcastle Upon Tyne, United Kingdom",
+				"dates" : "10/2010 - 01/2011",
+				"description" : "Design a part of the game experience"
+			},
+			{
+				"employer" : "Ubisoft",
+				"title" : "Game & Level Designer",
+				"location" : "Paris, France",
+				"dates" : "04/2009 - 09/2010",
+				"description" : "Design a part of the game mechanics & experience"
+			},
+			{
+				"employer" : "Kylotonn",
+				"title" : "Game & Level Designer",
+				"location" : "Paris, France",
+				"dates" : "04/2008 - 03/2009",
+				"description" : "Design a part of the game mechanics & experience"
+			}
+		],
+	},
+	"htmlString" : {
+		"HTMLheaderName" : '<h1 id="name">%data%</h1>',
+		"HTMLheaderRole" : '<div id="role">%data%</div><hr/>',
+
+		"HTMLcontactGeneric" : '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>',
+		"HTMLmobile" : '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>',
+		"HTMLemail" : '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>',
+		"HTMLtwitter" : '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>',
+		"HTMLgithub" : '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>',
+		"HTMLblog" : '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>',
+		"HTMLlocation" : '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>',
+
+		"HTMLbioPic" : '<img src="%data%" class="biopic">',
+		"HTMLwelcomeMsg" : '<span class="welcome-message">%data%</span>',
+
+		"HTMLskillsStart" : '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills"></ul>',
+		"HTMLskills" : '<li><span class="white-text">%data%</span></li>',
+
+		"HTMLworkStart" : '<div class="work-entry"></div>',
+		"HTMLworkEmployer" : '<a href="#">%data%',
+		"HTMLworkTitle" : ' - %data%</a>',
+		"HTMLworkDates" : '<div class="date-text">%data%</div>',
+		"HTMLworkLocation" : '<div class="location-text">%data%</div>',
+		"HTMLworkDescription" : '<p><br>%data%</p>',
+
+		"HTMLprojectStart" : '<div class="project-entry"></div>',
+		"HTMLprojectTitle" : '<a href="#">%data%</a>',
+		"HTMLprojectDates" : '<div class="date-text">%data%</div>',
+		"HTMLprojectDescription" : '<p><br>%data%</p>',
+		"HTMLprojectImage" : '<img src="%data%">',
+
+		"HTMLschoolStart" : '<div class="education-entry"></div>',
+		"HTMLschoolName" : '<a href="#">%data%',
+		"HTMLschoolDegree" : ' -- %data%</a>',
+		"HTMLschoolDates" : '<div class="date-text">%data%</div>',
+		"HTMLschoolLocation" : '<div class="location-text">%data%</div>',
+		"HTMLschoolMajor" : '<em><br>Major: %data%</em>',
+		"HTMLschoolURL" : '<br><a href="#">%data%</a>',
+
+		"HTMLonlineClasses" : '<h3>Online Classes</h3>',
+		"HTMLonlineTitle" : '<a href="#">%data%',
+		"HTMLonlineSchool" : ' - %data%</a>',
+		"HTMLonlineDates" : '<div class="date-text">%data%</div>',
+		"HTMLonlineURL" : '<br><a href="#">%data%</a>',
+
+		"googleMap" : '<div id="map"></div>',
+	}
+};
+
+var view = {
+	Init : function () {
+		view.DisplayBio($('#header'));
+		view.DisplayEducation();
+		view.DisplayWork();
+		view.DisplayProjects();
+		view.DisplayBio($('#footerContacts'));
+
+		var htmlString = octopus.getHtmlData();
+		$('#mapDiv').append(htmlString.googleMap);
+
+		view.AddSectionToggle();
+	},
+	DisplayBio : function (_element) {
+		var bio = octopus.getBioData();
+		var htmlString = octopus.getHtmlData();
+
+		if(_element.is($('#header'))) {
+			$('#header').prepend(htmlString.HTMLheaderRole.replace("%data%", bio.role));
+			$('#header').prepend(htmlString.HTMLheaderName.replace("%data%", bio.name));
+		}
+
+		if(_element.is($('#header'))) {
+			_contactElement = $('#topContacts');
+		}
+		else if(_element.is($('#footerContacts'))) {
+			_contactElement = $('#footerContacts');
+		}
+
+		_contactElement.append(htmlString.HTMLmobile.replace("%data%", bio.contacts.mobile));
+		_contactElement.append(htmlString.HTMLemail.replace("%data%", bio.contacts.email));
+		_contactElement.append(htmlString.HTMLtwitter.replace("%data%", bio.contacts.twitter));
+		_contactElement.append(htmlString.HTMLgithub.replace("%data%", bio.contacts.github));
+		_contactElement.append(htmlString.HTMLlocation.replace("%data%", bio.contacts.location));
+
+		if(_element.is($('#header'))) {
+			$('#header').append(htmlString.HTMLbioPic.replace("%data%", bio.biopic));
+			$('#header').append(htmlString.HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+
+			if(bio.skills.length > 0) {
+				$('#header').append(htmlString.HTMLskillsStart);
+				for (var i = 0; i < bio.skills.length; i++) {
+					$('#skills').append(htmlString.HTMLskills.replace("%data%", bio.skills[i]));
+				}
+			}
+		}
+	},
+	DisplayEducation : function () {
+		var education = octopus.getEducationData();
+		var htmlString = octopus.getHtmlData();
+
+		$('#education').append(htmlString.HTMLschoolStart);
+		for (school in education.schools) {
+			$('.education-entry:last').append(htmlString.HTMLschoolName.replace("%data%", education.schools[school].name) + htmlString.HTMLschoolDegree.replace("%data%", education.schools[school].degree));
+			$('.education-entry:last').append(htmlString.HTMLschoolDates.replace("%data%", education.schools[school].dates));
+			$('.education-entry:last').append(htmlString.HTMLschoolLocation.replace("%data%", education.schools[school].location));
+			for (major in education.schools[school].majors) {
+				$('.education-entry:last').append(htmlString.HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]));
+			}
+			$('.education-entry:last').append(htmlString.HTMLschoolURL.replace("%data%", education.schools[school].url));
+		}
+		$('.education-entry:last').append(htmlString.HTMLonlineClasses);
+		for (onlineCourse in education.onlineCourses) {
+			$('.education-entry:last').append(htmlString.HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title) + htmlString.HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school));
+			$('.education-entry:last').append(htmlString.HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates));
+			$('.education-entry:last').append(htmlString.HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url));
+		}
+	},
+	DisplayProjects : function () {
+		var projects = octopus.getProjectData();
+		var htmlString = octopus.getHtmlData();
+
+		for (project in projects.projects) {
+			$('#projects').append(htmlString.HTMLprojectStart);
+			$('.project-entry:last').append(htmlString.HTMLprojectTitle.replace("%data%", projects.projects[project].title));
+			$('.project-entry:last').append(htmlString.HTMLprojectDates.replace("%data%", projects.projects[project].dates));
+			$('.project-entry:last').append(htmlString.HTMLprojectDescription.replace("%data%", projects.projects[project].description));
+			for (image in projects.projects[project].images) {
+				$('.project-entry:last').append(htmlString.HTMLprojectImage.replace("%data%", projects.projects[project].images));
+			}
+		}
+    },
+    DisplayWork : function () {
+    	var work = octopus.getWorkData();
+		var htmlString = octopus.getHtmlData();
+
+		for (job in work.jobs) {
+			$('#workExperience').append(htmlString.HTMLworkStart);
+			$('.work-entry:last').append(htmlString.HTMLworkEmployer.replace("%data%", work.jobs[job].employer) + htmlString.HTMLworkTitle.replace("%data%", work.jobs[job].title));
+			$('.work-entry:last').append(htmlString.HTMLworkDates.replace("%data%", work.jobs[job].dates));
+			$('.work-entry:last').append(htmlString.HTMLworkLocation.replace("%data%", work.jobs[job].location));
+			$('.work-entry:last').append(htmlString.HTMLworkDescription.replace("%data%", work.jobs[job].description));
+		}
+    },
+    AddSectionToggle : function () {
+    	$('#workExperienceTitle').click(function () {
+	        $('#workExperience').animate({height: "toggle"}, "linear");
+	        $('#arrowWork').toggleClass('arrow-down');
+	    });
+	    $('#projectsTitle').click(function () {
+	        $('#projects').animate({height: "toggle"}, "linear");
+	        $('#arrowProjects').toggleClass('arrow-down');
+	    });
+	    $('#educationTitle').click(function () {
+	        $('#education').animate({height: "toggle"}, "linear");
+	        $('#arrowEducation').toggleClass('arrow-down');
+	    });
+	    $('#mapDivTitle').click(function () {
+	        $('#mapDiv').animate({height: "toggle"}, "linear");
+	        $('#arrowMap').toggleClass('arrow-down');
+	    });
+    }
+};
+
+var octopus = {
+	Init : function () {
+		view.Init();
+		octopus.map.AddListener();
+	},
+	getBioData : function () {
+		return model.bio;
+	},
+	getEducationData : function () {
+		return model.education;
+	},
+	getProjectData : function () {
+		return model.projects;
+	},
+	getWorkData : function () {
+		return model.work;
+	},
+	getHtmlData : function () {
+		return model.htmlString;
+	},
+	"map" : {
+		Init : function () {
+			var mapOptions = {
+				disableDefaultUI: true
+			};
+
+			/*
+			For the map to be displayed, the googleMap var must be
+			appended to #mapDiv in resumeBuilder.js.
+			*/
+			model.googlemaps.map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+			// Sets the boundaries of the map based on pin locations
+			window.mapBounds = new google.maps.LatLngBounds();
+
+			// locations is an array of location strings returned from locationFinder()
+			model.googlemaps.locations = octopus.map.LocationData();
+
+			// pinPoster(locations) creates pins on the map for each location in
+			// the locations array
+			octopus.map.PinPoster(model.googlemaps.locations);
+		},
+		AddListener : function () {
+
+			// Calls the initializeMap() function when the page loads
+			window.addEventListener('load', octopus.map.Init);
+
+			// Vanilla JS way to listen for resizing of the window
+			// and adjust map bounds
+			window.addEventListener('resize', function(e) {
+			  //Make sure the map bounds get updated on page resize
+			  model.googlemaps.map.fitBounds(mapBounds);
+			});
+		},
+		LocationData : function () {
+		    // initializes an empty array
+		    var locations = [];
+		    var bio = octopus.getBioData();
+		    var work = octopus.getWorkData();
+		    var education = octopus.getEducationData();
+
+		    // adds the single location property from bio to the locations array
+		    locations.push(bio.contacts.location);
+
+		    // iterates through school locations and appends each location to
+		    // the locations array
+		    for (var school in education.schools) {
+		    	//fixed the script to prevent spamy location list (google stopped answer after 8 identical requests)
+		    	if(locations.indexOf(education.schools[school].location) === -1) {
+		    		console.log("school added ==> " + education.schools[school].location);
+		    		locations.push(education.schools[school].location);
+		    	}
+		    }
+
+		    // iterates through work locations and appends each location to
+		    // the locations array
+		    for (var job in work.jobs) {
+		    	//fixed the script to prevent spamy location list (google stopped answer after 8 identical requests)
+		    	if(locations.indexOf(work.jobs[job].location) === -1) {
+		    		console.log("job added ==> " + work.jobs[job].location);
+		    		locations.push(work.jobs[job].location);
+		    	}
+			}
+
+			return locations;
+		},
+		CreateMarker : function (placeData) {
+
+		    // The next lines save location data from the search result object to local variables
+		    var lat = placeData.geometry.location.lat();  // latitude from the place service
+		    var lon = placeData.geometry.location.lng();  // longitude from the place service
+		    var name = placeData.formatted_address;   // name of the place from the place service
+		    var bounds = window.mapBounds;            // current boundaries of the map window
+
+		    // marker is an object with additional data about the pin for a single location
+		    var marker = new google.maps.Marker({
+		    	map: model.googlemaps.map,
+		    	position: placeData.geometry.location,
+		    	title: name
+		    });
+
+		    // infoWindows are the little helper windows that open when you click
+		    // or hover over a pin on a map. They usually contain more information
+		    // about a location.
+		    var infoWindow = new google.maps.InfoWindow({
+		    	content: name
+		    });
+
+		    // hmmmm, I wonder what this is about...
+		    google.maps.event.addListener(marker, 'click', function() {
+		    	infoWindow.open(model.googlemaps.map, marker);
+		    });
+
+		    // this is where the pin actually gets added to the map.
+		    // bounds.extend() takes in a map location object
+		    bounds.extend(new google.maps.LatLng(lat, lon));
+		    // fit the map to the new marker
+		   	model.googlemaps.map.fitBounds(bounds);
+		    // center the map
+		    model.googlemaps.map.setCenter(bounds.getCenter());
+		},
+		CallBack : function (results, status) {
+		    if (status == google.maps.places.PlacesServiceStatus.OK) {
+		    	octopus.map.CreateMarker(results[0]);
+		    	console.log(results[0]);
+		    }
+		},
+		PinPoster : function (locations) {
+		    // creates a Google place search service object. PlacesService does the work of
+		    // actually searching for location data.
+		    var service = new google.maps.places.PlacesService(model.googlemaps.map);
+
+		    // Iterates through the array of locations, creates a search object for each location
+		    for (var place in locations) {
+		    	console.log(locations[place]);
+		    	// the search request object
+		    	var request = {
+		    		query: locations[place]
+		    	};
+		    	//console.log(locations[place]);
+
+		    	// Actually searches the Google Maps API for location data and runs the callback
+		    	// function with the search results after each search.
+		    	service.textSearch(request, octopus.map.CallBack);
+		    }
+		}
+	}
+};
+
+octopus.Init();
